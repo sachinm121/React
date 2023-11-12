480ccd91ea2443f5da92cd7eb233c61ca7708c2e
sachinm121 <sachinmahor685@gmail.com>
2023-11-12 13:22:31 +0530

    react router dom

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-green-500 p-4'>React Router dom</div>
+      
+      {/* <Header/> */}
+    </>
+  )
+}
+
+export default App
